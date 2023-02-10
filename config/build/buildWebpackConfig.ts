68c729cb1f049c2e2765c608a9ca05ca16d05b65
Andrey Shi <andy.roshitu@gmail.com>
2023-02-10 04:04:16 +0300

    add: dev-server and update config for it

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,9 +1,9 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import path from "path";
 import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const {paths, mode} = options;
@@ -22,5 +22,6 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         },
         resolve: buildResolvers(),
         devtool: 'inline-source-map',
+        devServer: buildDevServer(options),
     };
 }
