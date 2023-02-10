ef9b991f6ad3d7dc3f16a0f45cdf5d8c10ce5922
Andrey Shi <andy.roshitu@gmail.com>
2023-02-11 02:39:39 +0300

    setting: add app-styles(global varibals, theme...)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import './index.scss';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Переключение темы</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
