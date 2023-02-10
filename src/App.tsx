ad16f58175c937fd8a1c6a3b2bb9de7b72627d39
Andrey Shi <andy.roshitu@gmail.com>
2023-02-11 00:07:12 +0300

    add: routing, react-router-dom and bundle chunks for lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import React, { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className='app'>
-            asdasdas
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
