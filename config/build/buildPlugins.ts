7b4c40fd2238be381fd429e9340dfe75d6dfc19f
Andrey Shi <andy.roshitu@gmail.com>
2023-02-16 15:17:36 +0300

    install: bundle analyze

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HTMLWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin, } from 'webpack-bundle-analyzer';
 import { BuildOptions, } from './types/config';
 
 export function buildPlugins({ paths, isDev, }: BuildOptions): webpack.WebpackPluginInstance[] {
@@ -19,6 +20,9 @@ export function buildPlugins({ paths, isDev, }: BuildOptions): webpack.WebpackPl
     }),
     isDev && new ReactRefreshWebpackPlugin({
       overlay: false,
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
     }),
   ].filter(Boolean);
 }
