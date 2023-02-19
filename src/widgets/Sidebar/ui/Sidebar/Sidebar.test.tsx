8ac1bcdd2dcd0e82cf4ae7d05d1bfaeed6a34d0a
Andrey Shi <andy.roshitu@gmail.com>
2023-02-19 19:23:11 +0300

    add: visual tool for ui testing; update: Sidebar, Button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,16 @@
 import { fireEvent, render, screen, } from '@testing-library/react';
 import { Sidebar, } from 'widgets/Sidebar';
 import { renderWithTranslation, } from 'shared/lib/renderWithTranslation/renderWithTranslation';
+import { ComponentRender, } from 'shared/lib/ComponentRender/ComponentRender';
 
 describe('Sidebar', () => {
   test('with only first param', () => {
-    renderWithTranslation(<Sidebar />);
+    ComponentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('test toggle', () => {
-    renderWithTranslation(<Sidebar />);
+    ComponentRender(<Sidebar />);
     const toggleBtn = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleBtn);
